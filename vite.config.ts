6cccedafbb43e85a268fd34c2f0473a64ffe5bc2
DendiRob <dendiroblek@gmail.com>
2024-02-26 13:02:17 +0400

    added some cvite conf changes

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
   build: {
     outDir: '../dist'
   },
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+  },
   server: {
     host: '0.0.0.0',
     port: 3000
